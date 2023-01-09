ad1b44ea21f20d19569b09da42d75a002c2f7c21
Michell2022 <sistemas.angel04@gmail.com>
2023-01-09 18:53:34 -0500

    estilo y logica terminado 2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,7 +8,7 @@ import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,6 +16,28 @@ export class HomeComponent {
   // CREO UNA VARIABLE PARA LA ETIQUETA OPTION QUE MOSTRARA UN QUE DEBE ELEGIR UN TIPO DE MODEDA
   opciones: string = "ninguna";
 
-
-
+  //CREANDO MI OBJETO 
+  myobjeto:any=[  
+    {
+      "Apellido":"Arcila",
+      "Casado":false,
+      "Dirección":"Calle 35 # 43 28",
+      "Nombre":"Diego",
+      "Teléfono":3859720
+    },
+    {
+      "Apellido":"Bueno",
+      "Casado":false,
+      "Dirección":"CR 16A # 53 28",
+      "Nombre":"Kevin",
+      "Teléfono":31485579954
+    },
+    {
+      "Apellido":"Palomino",
+      "Casado":false,
+      "Dirección":"CR 25 # 52 33",
+      "Nombre":"Natalia",
+      "Teléfono":32255945555
+    }
+  ];
 }
