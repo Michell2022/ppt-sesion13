4e6e26de2ae27a9d18fa7f00275e4a5acba81a3a
Michell2022 <sistemas.angel04@gmail.com>
2023-01-09 14:00:51 -0500

    diseño y logica terminado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,10 +7,15 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  // DECLARO MI VARIABLE DE TIPO STRING PARA EL MONTO QUE INGRESARA EL USUARIO
   palabra!:string;
 
+  // DECLARO MI VARIABLE DE TIPO NUMBER PARA EL MONTO QUE INGRESARA EL USUARIO
   monto!:number;
 
-  opciones!:string;
+  // CREO UNA VARIABLE PARA LA ETIQUETA OPTION QUE MOSTRARA UN QUE DEBE ELEGIR UN TIPO DE MODEDA
+  opciones: string = "ninguna";
+
+
 
 }
